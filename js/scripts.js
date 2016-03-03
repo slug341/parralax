2ec618aab1c286ed9f3dd2c9d230ad107e93cca1
Nickolas Caimi <10008189@sbschools.org>
2016-03-03 10:46:16 -0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+ /* Scripts for wave effect parallax and scrollspy demo */
+
+$(document).ready(function(){
+
+    /* For parallaxing images */
+    $('.parallax').parallax();
+
+    /* To scroll to a section from navbar */
+    $('.scrollspy').scrollSpy();
+});
